f736d6590a84f01975889bad0607f4f30eb91b73
Alfaza <alfazasatria8@gmail.com>
2019-10-17 14:10:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kalkulator/activities/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.kalkulator.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.kalkulator.R;
+import com.example.kalkulator.fragments.PersegiFragment;
+import com.example.kalkulator.fragments.PersegipanjangFragment;
+import com.example.kalkulator.fragments.SegitigaFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new PersegiFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+
+            case R.id.action_persegi:
+                fragment = new PersegiFragment();
+                break;
+            case R.id.action_persegipanjang:
+                fragment = new PersegipanjangFragment();
+                break;
+
+            case R.id.action_segitiga:
+                fragment = new SegitigaFragment();
+                break;
+
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
