dacee79c7e7a667f497db4c54b65b8dff7ac2cbf
Alfaza <alfazasatria8@gmail.com>
2019-10-17 20:17:51 +0700

    Add SplashActivity

--- app/src/main/java/com/example/kalkulator/activities/SplashActivity.java
+++ app/src/main/java/com/example/kalkulator/activities/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.kalkulator.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.kalkulator.R;
 
@@ -12,6 +14,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
+                finish();
+            }
+        },5000);
     }
 
 }
