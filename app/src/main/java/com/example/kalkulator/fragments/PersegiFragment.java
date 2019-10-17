e65247d7a63515b079154e1a7a00392e65a71985
Alfaza <alfazasatria8@gmail.com>
2019-10-17 14:22:32 +0700

    FragmentPersegiDone

--- app/src/main/java/com/example/kalkulator/fragments/PersegiFragment.java
+++ app/src/main/java/com/example/kalkulator/fragments/PersegiFragment.java
@@ -8,13 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.kalkulator.R;
+
+import java.text.NumberFormat;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PersegiFragment extends Fragment {
+
+    EditText input_sisi;
+    TextView hasilPersegi;
+    Button hitung_Persegi;
+    double sisi, hsk;
 
 
     public PersegiFragment() {
@@ -25,8 +35,22 @@ public class PersegiFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_persegi, container, false);
+        View view = inflater.inflate(R.layout.fragment_persegi, container, false);
+        input_sisi =  view.findViewById(R.id.edt_sisi);
+        hasilPersegi = view.findViewById(R.id.tv_result_persegi);
+        hitung_Persegi = view.findViewById(R.id.btn_calculate_persegi);
+
+        hitung_Persegi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sisi = Double.valueOf(input_sisi.getText().toString());
+                hsk = (sisi * sisi);
+                hasilPersegi.setText(NumberFormat.getInstance().format(hsk));
+
+            }
+
+        });
+        return view;
     }
 
 }
